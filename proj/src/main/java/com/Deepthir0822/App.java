92c064651c11ce9bda6c7b30132079ccb58bb6b8
Deepthir0822 <deepthir0828@gmail.com.com>
2025-03-14 10:54:40 +0530

    azure pipeline example

---
+++ proj/src/main/java/com/Deepthir0822/App.java
@@ -0,0 +1,13 @@
+package com.Deepthir0822;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
